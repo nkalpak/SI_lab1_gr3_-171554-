9730c8a862b141839df9f58246e64b091487e840
nkalpak <nikitakalpakovski1@outlook.com>
2020-04-08 17:24:24 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -4,6 +4,25 @@ class Item {
 	double price;
 	char ddv;
 
+	public Item(int id, String name, double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	double getTaxReturn() {
+		double ddvPercentage = getPercentageFromDdvClass(this.ddv);
+		double withDdv = price * ddvPercentage;
+
+		return 0.15 * withDdv;
+	}
+
+	double getPercentageFromDdvClass(char ddv) {
+		if (ddv == 'A') return 0.18;
+		if (ddv == 'B') return 0.05;
+		if (ddv == 'V') return 0.0;
+	}
+
 	//TODO add variable.
 
 	//TODO setters and getters
